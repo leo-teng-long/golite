da8f629488ef9d16b4ee756494e8665addf05229
Hardik Vala <hardikvala24@gmail.com>
2016-02-16 14:59:18 -0500

    Modified to parse

--- src/run.sh
+++ src/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ###
-Runs the scanner through all programs in the programs folder.
+Runs the parser through all programs in the programs folder.
 ###
 
 for prog in $(find ../programs -name "*.go" )
 do
 	echo "Testing $prog..."
-	java golite.Main $prog
+	java golite.Main -parse $prog
 done
