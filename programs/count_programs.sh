90f634e4f68fe9b3215932247d79c36ed3326688
Hardik Vala <hardikvala24@gmail.com>
2016-02-21 18:43:20 -0500

    Created script for counting programs

---
+++ programs/count_programs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+valid_prog_cnt=$(find valid/ -name "*.go" | wc -l | tr -d '[[:space:]]')
+invalid_prog_cnt=$(find invalid/ -name "*.go" | wc -l | tr -d '[[:space:]]')
+total_cnt=$((valid_prog_cnt + invalid_prog_cnt))
+
+echo "Valid: $valid_prog_cnt"
+echo "Invalid: $invalid_prog_cnt"
+echo "Total: $total_cnt"
+
