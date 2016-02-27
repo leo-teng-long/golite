74b7d239bce5322f6ebd82a958754f22025ba224
Ethan Macdonald <ethan.bryce.macdonald@gmail.com>
2016-02-26 19:48:50 -0500

    Flesh out ifs test

--- programs/valid/general/ifs.go
+++ programs/valid/general/ifs.go
@@ -9,18 +9,35 @@ func main() {
 	if x {
 		//Do nothing
 	}
-	if z:= true; z {
-		//Do nothing
-	}
+
 	if x {
 		//Do nothing
 	} else {
 		//Do nothing
 	}
+
 	if y {
 		//Do nothing
 	} else if x {
 		//Do nothing
+	} else {
+		//Do nothing
+	}
+
+	if z:= true; z {
+		println(z)
+	}
+
+	if z:= true; x {
+		println(z)
+	} else {
+		//Do nothing
+	}
+	
+	if z:= true; y {
+		println(z)
+	} else if z:= true; x {
+		println(z)
 	} else {
 		//Do nothing
 	}
