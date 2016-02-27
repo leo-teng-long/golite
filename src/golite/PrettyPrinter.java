67585b829fe8d42328372f9ae8aed98203f0f34a
Ethan Macdonald <ethan.bryce.macdonald@gmail.com>
2016-02-26 22:14:25 -0500

    Fix double printing struct error

--- src/golite/PrettyPrinter.java
+++ src/golite/PrettyPrinter.java
@@ -202,9 +202,9 @@ public class PrettyPrinter extends DepthFirstAdapter {
     }
     if (node.getTypeExpr() != null) {
       addSpace();
-      if (node.getTypeExpr() instanceof AStructTypeExpr) {
+      /*if (node.getTypeExpr() instanceof AStructTypeExpr) {
         buffer.append("struct");
-      }
+      }*/
       node.getTypeExpr().apply(this);
     }
   }
