744d10749a068d9cdbcbdac81fcb298e81b72e56
Hardik Vala <hardikvala24@gmail.com>
2016-03-20 16:49:00 -0400

    Moved braced_case_block.go test to valid

--- programs/valid/syntax/braced_case_block.go
+++ programs/valid/syntax/braced_case_block.go
@@ -7,7 +7,7 @@ func main() {
 
 	switch x {
 		default: println("Error: Single digit required")
-		case 0, 2, 4, 6, 8: { // Error: case blocks don't involve braces.
+		case 0, 2, 4, 6, 8: {
 			println("Even")
 		}
 		case 1, 3, 5, 7, 9: println("Odd")
