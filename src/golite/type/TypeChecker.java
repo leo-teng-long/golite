a66146931da2a3eacdd62afb424112f24b2a53f1
Leo Long <leo.teng.long@gmail.com>
2016-03-22 21:55:46 -0400

    Check the number of arguments for alias type casting

--- src/golite/type/TypeChecker.java
+++ src/golite/type/TypeChecker.java
@@ -1507,6 +1507,9 @@ public class TypeChecker extends DepthFirstAdapter {
         else if (decl instanceof ASpecTypeSpec)
         {
             PTypeExpr castType = ((ASpecTypeSpec) decl).getTypeExpr();
+            if (node.getExpr().size() != 1) {
+                callTypeCheckException(node, "Alias type casting: too few or too many arguments");
+            }
             if    (castType instanceof AIntTypeExpr
             || castType instanceof AFloatTypeExpr
             || castType instanceof ARuneTypeExpr
@@ -1709,4 +1712,5 @@ public class TypeChecker extends DepthFirstAdapter {
         callTypeCheckException(node, "Did not find type for " + node.getClass() + " tried to return null");
         return null;
     }
+    
 }
