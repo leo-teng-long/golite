4ceb66748b1194a658afd9dc0ee2265c71c7850c
Hardik Vala <hardikvala24@gmail.com>
2016-03-26 01:52:04 -0400

    Created TypeException

---
+++ src/golite/exception/TypeException.java
@@ -0,0 +1,14 @@
+package golite.exception;
+
+
+/**
+ * Type exception.
+ */
+@SuppressWarnings("serial")
+public class TypeException extends RuntimeException {
+
+    public TypeException(String message) {
+        super(message);
+    }
+    
+}
