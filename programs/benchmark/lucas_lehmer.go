003eb3b465d86db2bef8ed1002fb3ffbf2ee6e0b
Hardik Vala <hardik.vala@mail.mcgill.ca>
2016-04-04 16:09:29 -0400

    Fixed Lucas-Lehmer benchmark

--- programs/benchmark/lucas_lehmer.go
+++ programs/benchmark/lucas_lehmer.go
@@ -1,7 +1,11 @@
-/* Lucas-Lehmer test for testing the primality of Mersenne primes. */
+/* Lucas-Lehmer test for testing the primality of Mersenne primes (i.e. Primes
+ * of the form 2^p - 1 for some integer p). */
 
+package main
+
+// Calculate x^n.
 func exp(x, n int) int {
-	var r int = 0
+	var r int = 1
 	for i := 0 ; i < n ; i++ {
 		r *= x
 	}
@@ -9,9 +13,9 @@ func exp(x, n int) int {
 	return r
 }
 
-func LucasLehmer(int p) bool {
-	s = 0
-	m = exp(2, p) - 1
+// Apply the Lucas Lehmer test to check whether 2^p - 1 is prime or not.
+func LucasLehmer(p int) bool {
+	var s, m int = 4, exp(2, p) - 1
 
 	for i := 0 ; i < p - 2 ; i++ {
 		s = (exp(s, 2) - 2) % m
@@ -20,6 +24,12 @@ func LucasLehmer(int p) bool {
 	return s == 0
 }
 
+// Test whether 2^2018796950 is prime or not. Runs in approximately 14 sec.
+// using Go.
 func main() {
-	println(LucasLehmer(74207281))
+	if LucasLehmer(2018796950) {
+		println("Is prime!")
+	} else {
+		println("Not prime :(")
+	}
 }
